fa7587160d9e1cc97ade8f9a0fe146d334473906
mdlfing3r <mdlfing3r@gmail.com>
2022-11-24 22:15:26 +0600

    Добавьте файлы проекта.

---
+++ Source.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <time.h>
+#include <stdlib.h>
+
+
+std::string getpass(int, bool, bool);
+
+
+int main()
+{
+
+
+
+
+	return 0;
+}
+
+
+std::string getpass(int length, bool AddSymbols, bool);
